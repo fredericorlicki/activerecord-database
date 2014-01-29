fc85e68952641a85d93400522ad9c99501e5290a
Mac Mob <Macmob@MacBook-Pro-de-Mac.local>
2014-01-29 11:44:08 +0100

    Add forgotten main.rb file to version control

--- 01.Interacting_with_code/main.rb
+++ 01.Interacting_with_code/main.rb
@@ -4,8 +4,9 @@ require 'sqlite3'
 database_path = "db/jukebox.sqlite"
 db = SQLite3::Database.new(database_path)
 
-def number_of_rows(db,table_name)
-  # your code here
+def number_of_rows(db, table_name)
+	n = db.execute("SELECT count(*) FROM #{table_name}")
+   n[0][0]
 end
 
 puts "There are #{number_of_rows(db,"Artist")} artists"
@@ -16,10 +17,18 @@ puts "There are #{number_of_rows(db,"MediaType")} media types"
 
 
 # returns all the artists sorted by alphabetical order.
-artists = # your code here
 
+def sort(db, field, table_name)
+	db.execute("SELECT #{field} FROM #{table_name} ORDER BY #{field}")
+end
+artists = sort(db, "name", "Artist")
+
+puts "Here is the list: #{artists} "
+
+#puts "Here is the list #{alphabetical_sort(db,"Artist")} "
 # returns all the love songs
-love_songs = # your code here
+love_songs = db.execute("SELECT Track.name, Album.title FROM Track INNER JOIN Album ON Track.albumId = Album.albumId  WHERE Track.name LIKE '%love%' ") # your code here
+p love_songs
 
 # returns all the tracks that are longer than 5 minutes.
-long_tracks = # your code here
+#long_tracks = # your code here
